3842d8b55afcfdce0d805b6be2bc09d736d36850
hemaro <hemaro@aistechnolabs.biz>
2021-08-16 09:48:43 +0530

    commit

---
+++ src/Hemarao/laravelcdnv2/Exceptions/Exceptions.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Hemarao\laravelCDNv2\Exceptions;
+
+/**
+ * @author Hemarao <hemsbapu9644@gmail.com>
+ */
+class CdnException extends \RuntimeException
+{
+}
+
+class MissingConfigurationFileException extends CdnException
+{
+}
+
+class MissingConfigurationException extends CdnException
+{
+}
+
+class UnsupportedProviderException extends CdnException
+{
+}
+
+class EmptyPathException extends CdnException
+{
+}
