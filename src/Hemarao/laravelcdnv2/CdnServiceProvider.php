82fcbed350e615700f6b995ed8a99fc6279dea45
hemaro <hemaro@aistechnolabs.biz>
2021-08-19 17:52:45 +0530

    commit

--- src/Hemarao/laravelcdnv2/CdnServiceProvider.php
+++ src/Hemarao/laravelcdnv2/CdnServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Hemarao\Laravelcdn\laravelcdnv2;
+namespace Hemarao\laravelcdnv2;
 
 use Illuminate\Support\ServiceProvider;
 use Hemarao\laravelcdn\laravelcdnv2\Contracts\CdnInterface;
